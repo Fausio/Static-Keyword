cf8eb616b007eb65bea483fb474e54c5dcc7904a
fausio luis <fausioluis@live.com>
2020-04-04 20:55:29 +0200

    statinc :Field - Constructor - Property - Method - Class

--- Static_Keyword/HousingLoanCustomer.cs
+++ Static_Keyword/HousingLoanCustomer.cs
@@ -9,12 +9,16 @@ namespace Static_Keyword
         public int CustomerId { get; set; }
         public string Name { get; set; }
         public double LoanAmount { get; set; }
-        static double ROI { get; set; }
-
+        static double _ROI { get; set; }
+        public static double ROI
+        {
+            get { return _ROI; }
+            set { _ROI = value; }
+        }
 
         static HousingLoanCustomer()
         {
-            ROI = 7.5;
+            _ROI = 7.5;
         }
         public HousingLoanCustomer(int CustomerId, string Name, double LoanAmount)
         {
@@ -23,9 +27,23 @@ namespace Static_Keyword
             this.LoanAmount = LoanAmount;
         }
 
-        public void CalculateIntrest()
+        public void CalculateIntrest(int months)
         {
-            double Intrest = (LoanAmount * ROI) / 100;
+            double Intrest = (LoanAmount * _ROI) / 100;
+            double RepayAmount = Intrest + LoanAmount;
+
+            Console.WriteLine("Loan Amount:{0} IntAmount:{1} RepayAmount:{2} ROI:{3}", LoanAmount, Intrest, RepayAmount, _ROI);
+            Console.WriteLine("EMI:{0}", (RepayAmount / months));
+        }
+
+        public static void Enquery(double EnloanAmout, int months)
+        {
+            Console.WriteLine("ROI:{0}", ROI);
+            Console.WriteLine("Loan Amount:{ 0}", EnloanAmout);
+            Console.WriteLine("IntAmount:{ 0}", (EnloanAmout * ROI / 100));
+            Console.WriteLine("RepayAmount:{ 0}", (EnloanAmout + (EnloanAmout * ROI / 100)));
+            Console.WriteLine("EMI Months:{ 0}", months);
+            Console.WriteLine("EMI:{ 0}", (EnloanAmout + (EnloanAmout * ROI / 100)) / months);
         }
     }
 }
--- Static_Keyword/Program.cs
+++ Static_Keyword/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Static_Keyword;
 
 namespace Static_Keyword
 {
@@ -6,7 +7,20 @@ namespace Static_Keyword
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var LoadnCustomer1 = new HousingLoanCustomer(1, "Fáusio", 4114);
+            var LoadnCustomer2 = new HousingLoanCustomer(2, "Luís", 1000);
+
+            LoadnCustomer1.CalculateIntrest(1);
+            LoadnCustomer2.CalculateIntrest(1);
+
+            HousingLoanCustomer.ROI = 1.5;
+
+            LoadnCustomer1.CalculateIntrest(1);
+            LoadnCustomer2.CalculateIntrest(1);
+
+            Console.ReadKey();
+ 
+                
         }
     }
 }
