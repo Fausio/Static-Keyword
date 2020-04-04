cf67ee802d9716ed0bce7336fc3ef68a7fe9c9db
fausio luis <fausioluis@live.com>
2020-04-04 19:56:31 +0200

    creating class  Housing Loan Customer

---
+++ Static_Keyword/HousingLoanCustomer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Keyword
+{
+    public class HousingLoanCustomer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public double LoanAmount { get; set; }
+        public double ROI { get; set; }
+
+
+        public HousingLoanCustomer(int CustomerId, string Name, double LoanAmount, double ROI)
+        {
+            this.CustomerId = CustomerId;
+            this.Name = Name;
+            this.LoanAmount = LoanAmount;
+            this.ROI = ROI;
+        }
+    }
+}
