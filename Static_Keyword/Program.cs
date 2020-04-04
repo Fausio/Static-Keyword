4df63556e15ffa6c9b9cba0a946116abcdb7ea72
fausio luis <fausioluis@live.com>
2020-04-04 19:46:40 +0200

    Add project files.

---
+++ Static_Keyword/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Static_Keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
